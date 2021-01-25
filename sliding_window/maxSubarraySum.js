417879a0c13d7a3266c15d2e77ec5104800979b1
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-01-24 19:41:22 -0800

    sliding method

--- sliding_window/maxSubarraySum.js
+++ sliding_window/maxSubarraySum.js
@@ -16,3 +16,29 @@ for (let i = 0; i < arr.length - num + 1; i ++ ){
 }
     return max
 }
+
+maxSubarrySum([2,3,4,5], 3)
+
+// sliding window approach
+
+function maxSubarrySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    // create first sum
+    for (let i = 0; i < num; i++){
+     // store sum in variable
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        // start loop from tempsum
+        tempSum = tempSum - arr[i - num] + arr[i];
+        // takes maximum between maxSum and tempSum
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum 
+}
+
+// time c o(n)
+// array [1,2,3,4,5,6,] subtract previous number and add the next 
